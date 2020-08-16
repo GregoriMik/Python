1124ec0168d8298e5a5931ccadca5a670d044169
Gregori Mikina <gregorimikina@gmail.com>
2020-08-16 15:23:59 +0200

    Draw six numbers from 1 to 49

---
+++ 17/README.md
@@ -0,0 +1,2 @@
+[Pl]Program losuje sześć liczb od 1 do 49
+[Eng]The program draws six numbers from 1 to 49
---
+++ 17/main.py
@@ -0,0 +1,5 @@
+# 1 do 49, 6 liczb
+# wylosuj 6 liczb z dużego lotka
+import random as r
+for i in range(6):
+    print(r.randrange(1, 50))
