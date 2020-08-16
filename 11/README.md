778602678fa62932df46dc31dff7435f3bf5d86d
Gregori Mikina <gregorimikina@gmail.com>
2020-08-16 15:14:41 +0200

    Read the word and check is the word entered akademia

---
+++ 11/README.md
@@ -0,0 +1,5 @@
+Program odczytuje wyraz i sprawdza
+czy wprowadzony wyraz to Akademia.
+
+Program reads the word and check
+is the word entered akademia.
---
+++ 11/main.py
@@ -0,0 +1,5 @@
+word = input('Podaj wyraz\n')
+if word.lower() == 'akademia':
+    print('Podałeś poprawne hasło')
+else:
+    print('Podałeś niepoprawne hasło')
