0e1ccab0111c5bb5809dc7c47cb1b4d83aa78fe6
Gregori Mikina <gregorimikina@gmail.com>
2020-08-16 15:19:31 +0200

    Write ten stars in three ways

---
+++ 15/README.md
@@ -0,0 +1,2 @@
+[Pl]Program drukuję na ekranie 10 gwiazdek \n
+[Eng]The program prints 10 stars on the screen
---
+++ 15/main.py
@@ -0,0 +1,7 @@
+# sposób 1
+print('*'*10)
+# sposób 2
+print('**********')
+# sposób 3
+for i in range(1, 11):
+    print('*')
