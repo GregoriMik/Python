0eef2894a5e16d163d64a7f1460095b6355d5485
Gregori Mikina <gregorimikina@gmail.com>
2020-08-16 15:17:55 +0200

    Program print numbers up to 20 to 1

---
+++ 14/README.md
@@ -0,0 +1,2 @@
+[Pl]Program wypisuję liczby do 20 do 1\n
+[Eng]The program prints numbers up to 20 to 1
---
+++ 14/main.py
@@ -0,0 +1,7 @@
+# Pierwszy element- od którego zaczynamy
+# Drugi element-element końca
+# Trzeci element- o ile ma się zmieniać
+
+for i in range(20, 0, -1):
+    print(i)
+print('Koniec')
