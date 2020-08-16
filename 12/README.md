7a72ac6472f8f9747431b9dab2460479cc0422bc
Gregori Mikina <gregorimikina@gmail.com>
2020-08-16 15:16:26 +0200

    Check if the person is of legal age

---
+++ 12/README.md
@@ -0,0 +1,2 @@
+[PL]Sprawdź czy dana osoba jest już pełnoletnia \n
+[ENG]Check if the person is of legal age
---
+++ 12/main.py
@@ -0,0 +1,5 @@
+age = int(input('Podaj swój wiek\n'))
+if age >= 18:
+    print('Jesteś osobą pełnoletnią')
+else:
+    print('Jesteś dzieckiem')
